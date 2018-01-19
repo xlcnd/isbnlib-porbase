bc716e986bfc3bb145b5a32b1ef7db9d0d50bf89
Alexandre Conde <alexandreconde@hotmail.com>
2018-01-19 16:50:54 +0000

    delete integration test

--- isbnlib_porbase/test/test_metadata.py
+++ isbnlib_porbase/test/test_metadata.py
@@ -3,8 +3,9 @@
 # pylint: skip-file
 """nose tests for metadata."""
 
-from .._porbase import query
 from nose.tools import assert_equals, assert_raises
+from isbnlib import meta
+from .._porbase import query
 
 
 def test_query():
@@ -15,3 +16,9 @@ def test_query():
     assert_equals(len(repr(query('9789720049612'))) > 100, True)
     assert_equals(len(repr(query('9789727228133'))) > 100, True)
     assert_raises(Exception, query, '9780000000')
+
+
+# def test_ext_meta():
+#    """Test the query of metadata (porbase.org) with 'high level' meta function."""
+#    # test meta from core
+#    assert_equals(len(repr(meta('9789727576807', 'porbase'))) > 100, True)
