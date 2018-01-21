e45a4d56d0c08a258cdf4b00f1ea34f35a9ef952
Alexandre Conde <alexandreconde@hotmail.com>
2018-01-21 14:44:48 +0000

    use builtin function (isdigit)

--- isbnlib_porbase/_porbase.py
+++ isbnlib_porbase/_porbase.py
@@ -39,7 +39,7 @@ def parser_porbase(xml):
         recs[key] = u(txt)
     recs['Publisher'] = recs.get('Publisher', u('')).split('|')[0]
     recs['Authors'] = recs.get('Authors', u([])).split('|')
-    recs['Year'] = ''.join([c for c in recs.get('Year', u('')) if c.isdigit()])
+    recs['Year'] = ''.join(c for c in recs.get('Year', u('')) if c.isdigit())
     return recs
 
 
