512ecb3daef98379f92c25e64cbe575d5bcac9cc
Alexandre Conde <alexandreconde@hotmail.com>
2018-01-22 12:53:16 +0000

    init v0.1.0

--- isbnlib_porbase/__init__.py
+++ isbnlib_porbase/__init__.py
@@ -4,4 +4,4 @@
 from ._porbase import query
 
 
-__version__ = '0.0.9'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author='xlcnd',
     author_email='xlcnd@outlook.com',
     url='https://github.com/xlcnd/isbnlib-porbase',
-    download_url='https://github.com/xlcnd/isbnlib-porbase/archive/v0.0.9.zip',
+    download_url='https://github.com/xlcnd/isbnlib-porbase/archive/v0.1.0.zip',
     packages=['isbnlib_porbase/'],
     entry_points={'isbnlib.metadata': ['porbase=isbnlib_porbase:query']},
     install_requires=["isbnlib>=3.7.3,<3.8.0"],
