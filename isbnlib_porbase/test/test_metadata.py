9d1ab1a39640000aa0bcdfba0326177dd715be6a
Alexandre Conde <alexandreconde@hotmail.com>
2019-02-20 10:46:11 +0000

    split tests

--- isbnlib_porbase/test/test_metadata.py
+++ isbnlib_porbase/test/test_metadata.py
@@ -9,14 +9,20 @@ from .._porbase import query
 
 
 def test_query():
-    """Test the query of metadata (porbase.org) with 'low level' queries."""
-    assert_equals(len(repr(query('9781849692341'))) == 2, True)
-    assert_equals(len(repr(query('9781849692343'))) == 2, True)
-
+    """Test porbase.org with 'low level' queries."""
     assert_equals(len(repr(query('9789727576807'))) > 100, True)
     assert_equals(len(repr(query('9789720049612'))) > 100, True)
     assert_equals(len(repr(query('9789727228133'))) > 100, True)
 
+
+def test_query_missing():
+    """Test porbase.org with 'low level' queries (missing data)."""
+    assert_equals(len(repr(query('9781849692341'))) == 2, True)
+    assert_equals(len(repr(query('9781849692343'))) == 2, True)
+
+
+def test_query_wrong():
+    """Test porbase.org with 'low level' queries (wrong data)."""
     assert_equals(len(repr(query('9780000000'))) == 2, True)
 
 
